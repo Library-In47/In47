5adf5504673f16042a3ce4ea1e2cbb91e007c588
NicoLoreto <nicoloreto.dev@gmail.com>
2024-05-06 19:28:17 -0300

    Resolved conflicts

--- web project/front/library-in47/src/app/user/user/user.component.ts
+++ web project/front/library-in47/src/app/user/user/user.component.ts
@@ -4,7 +4,6 @@ import { Order } from 'src/app/models/order';
 import { AuthService } from 'src/app/services/auth.service';
 import { OrderService } from 'src/app/services/orden.service';
 
-
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -14,18 +13,13 @@ export class UserComponent implements OnInit {
   public user?: User;
   orders: Order[] | null = null;
 
-  constructor(private authService: AuthService, private orderService: OrderService) { }
+  constructor(
+    private authService: AuthService,
+    private orderService: OrderService
+  ) {}
 
   ngOnInit(): void {
     this.user = this.authService.getUser();
-    this.authService.getProfile().subscribe(
-      (data) => {
-        this.user = data;
-      },
-      (error) => {
-        console.error('Error obteniendo los datos del usuario', error);
-      }
-    );
 
     this.orderService.getAllOrdenes().subscribe(
       (data) => {
