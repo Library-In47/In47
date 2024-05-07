2850e7f46edc9f80f3a8b377edf9f2c882f57741
Gaston Paez <paez.gastonm@gmail.com>
2024-05-07 00:23:40 -0300

    Se añade filtro para la busqueda mediante el back y servicio para busqueda desde el front

--- web project/front/library-in47/src/app/header/header.component.ts
+++ web project/front/library-in47/src/app/header/header.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+// header.component.ts
+import { Component, OnInit } from '@angular/core';
+import { SearchService } from 'src/app/services/search.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  searchText: string = '';
 
+  constructor(private searchService: SearchService) { }
+
+  ngOnInit(): void {
+  }
+
+  search(): void {
+    this.searchService.setSearchText(this.searchText);
+  }
 }
--- web project/front/library-in47/src/app/home/home/home.component.ts
+++ web project/front/library-in47/src/app/home/home/home.component.ts
@@ -1,6 +1,9 @@
+// home.component.ts
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
+import { SearchService } from 'src/app/services/search.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,13 +11,44 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  libros !: Product[];
+  libros: Product[] = [];
+  librosFiltrados: Product[] = [];
+  searchText: string = '';
+  private searchTextSubscription: Subscription;
 
-  constructor(private productService:ProductService){ }
+  constructor(
+    private productService: ProductService,
+    private searchService: SearchService
+  ) {
+    this.searchTextSubscription = new Subscription(); // Inicialización en el constructor
+  }
 
   ngOnInit(): void {
     this.productService.getAll().subscribe(
-      libro => this.libros=libro
-      );  
+      libros => {
+        this.libros = libros;
+        this.filterBooks();
+      }
+    );
+
+    this.searchTextSubscription = this.searchService.searchText$.subscribe(
+      searchText => {
+        this.searchText = searchText;
+        this.filterBooks();
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.searchTextSubscription.unsubscribe();
+  }
+
+  filterBooks(): void {
+    this.librosFiltrados = this.libros.filter(libro =>
+      libro.titulo.toLowerCase().includes(this.searchText.toLowerCase()) ||
+      libro.subtitulo.toLowerCase().includes(this.searchText.toLowerCase()) ||
+      libro.isbn.toLowerCase().includes(this.searchText.toLowerCase()) ||
+      libro.resenia.toLowerCase().includes(this.searchText.toLowerCase())
+    );
   }
 }
